a8170edf1734bae16db2efe9491eeec145de5bfa
Mikael Messias <mikaelmessias@outlook.com>
2020-06-23 17:56:52 -0300

    Add dotenv
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import routes from './routes';
+import env from './utils/dotenv';
 
 const app = express();
 
@@ -8,4 +9,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(env.PORT);
