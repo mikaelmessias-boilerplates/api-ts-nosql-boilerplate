56e5b1362af1349a4df5bcd2f6eb1b9cbe239808
Mikael Messias <mikaelmessias@outlook.com>
2020-06-23 20:05:37 -0300

    Add an example route for image upload
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/routes.ts
+++ src/routes.ts
@@ -6,9 +6,11 @@ import UserController from './controllers/User';
 const userController = new UserController();
 
 const routes = express.Router();
+// const upload = multer(multerConfig);
 
 routes.get('/users', userController.index);
 routes.get('/user/:userId', userController.show);
 routes.post('/user', userController.store);
+// routes.post('/', upload.single('image'), route);
 
 export default routes;
